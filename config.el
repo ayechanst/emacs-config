acd38042748ac023b8fa511bd51183b5e46afa54
Aye Chan <ayechan.sant@gmail.com>
2024-01-29 11:39:58 -0800

    toc and bullets

--- config.el
+++ config.el
@@ -71,11 +71,6 @@
 (setq evil-collection-mode-list '(dashboard dired ibuffer))
 (evil-collection-init))
 (use-package evil-tutor)
-
-
-
-
-
 ;;Turns off elpaca-use-package-mode current declaration
 ;;Note this will cause the declaration to be interpreted immediately (not deferred).
 ;;Useful for configuring built-in emacs features.
@@ -92,7 +87,7 @@
     :keymaps 'override
     :prefix "SPC" ;; set leader key
     :global-prefix "M-SPC")
-  
+
   (ayechan/leader-keys 
     "b" '(:ignore t :wk "buffer") ;; sets the next key (in this case b)
     ;; "keys" '(elisp-function :wk(which key) "name")
@@ -100,34 +95,69 @@
     "bk" '(kill-this-buffer :wk "Kill this buffer")
     "bn" '(next-buffer :wk "Next buffer")
     "bp" '(previous-buffer :wk "Previous buffer")
-    "br" '(revert-buffer :wk "Reload buffer")))
+    "br" '(revert-buffer :wk "Reload buffer")
 
-(set-face-attribute 'default nil
-                    :font "JetBrains Mono"
-                    :height 110 
-                    :weight 'medium)
+    "f" '(:ignore t :wk "file") ;; sets the next key
+    "ff" '(find-file :wk "Find file")))
 
-(set-face-attribute 'variable-pitch nil
-                    :font "Ubuntu"
-                    :height 120
-                    :weight 'medium)
+;; (set-face-attribute 'default nil
+;;                     :font "JetBrains Mono"
+;;                     :height 110 
+;;                     :weight 'medium)
 
-(set-face-attribute 'fixed-pitch nil
-                    :font "JetBrains Mono"
-                    :height 110
-                    :weight 'medium)
+;; (set-face-attribute 'variable-pitch nil
+;;                     :font "Ubuntu"
+;;                     :height 120
+;;                     :weight 'medium)
 
-;; Make commented text and keywords italic.
-;; This is working in emacsclient but not in emacs.
-;; Your font must have an italic face available.
-(set-face-attribute 'font-lock-comment-face nil
-                    :slant 'italic)
+;; (set-face-attribute 'fixed-pitch nil
+;;                     :font "JetBrains Mono"
+;;                     :height 110
+;;                     :weight 'medium)
 
-(set-face-attribute 'font-lock-keyword-face nil
-                    :slant 'italic)
+;; ;; Make commented text and keywords italic.
+;; ;; This is working in emacsclient but not in emacs.
+;; ;; Your font must have an italic face available.
+;; (set-face-attribute 'font-lock-comment-face nil
+;;                     :slant 'italic)
 
-;; Set font on graphical frames
-(add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
+;; (set-face-attribute 'font-lock-keyword-face nil
+;;                     :slant 'italic)
 
-;; Uncomment the following line if line spacing needs adjusting
-(setq-default line-spacing 0.12)
+;; ;; Set font on graphical frames
+;; (add-to-list 'default-frame-alist '(font . "JetBrains Mono-11"))
+
+;; ;; Uncomment the following line if line spacing needs adjusting
+;; (setq-default line-spacing 0.12)
+
+(use-package toc-org
+    :commands toc-org-enable
+    :init (add-hook 'org-mode-hook 'toc-org-enable))
+
+(add-hook 'org-mode-hook #'org-indent-mode)
+(use-package org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+
+(global-display-line-numbers-mode 1)
+(global-visual-line-mode t)
+
+(use-package which-key
+:init 
+(which-key-mode 1)
+:config
+(setq which-key-side-window-location 'bottom
+      which-key-sort-order #'which-key-key-order-alpha
+      which-key-sort-uppercase-first nil
+      which-key-add-column-padding 1
+      which-key-max-display-columns nil
+      which-key-min-display-lines 6
+      which-key-side-window-slot -10
+      which-key-side-window-max-height 0.25
+      which-key-idle-delay 0.8
+      which-key-max-description-length 25
+      which-key-allow-imprecise-window-fit t
+      which-key-separator " → " ))
---
+++ elpaca/builds/which-key/which-key.el
@@ -0,0 +1,1 @@
+/home/santun/.config/emacs/elpaca/repos/emacs-which-key/which-key.el
